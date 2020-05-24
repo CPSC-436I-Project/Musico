0276b658e5ba796d11e9a1ede4260f6ffb54a6ef
Perry Liao <perryliaohc@gmail.com>
2020-05-24 14:34:37 -0700

    Create abstract container class

---
+++ app/src/containers/Container.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import {ReactNode} from "react";
+
+abstract class Container<P extends IContainerProps = IContainerProps, S extends IContainerState = IContainerState> extends React.PureComponent<P, S> {
+
+	public static defaultProps: IContainerProps = {
+		/* default props to all containers here */
+	};
+
+	private readonly childRender: () => ReactNode;
+
+	protected constructor(props: P) {
+		super(props);
+
+		this.wrapRender = this.wrapRender.bind(this);
+
+		// @ts-ignore
+		this.state = {};
+
+		this.childRender = this.render;
+		this.wrapRender();
+	}
+
+	private wrapRender(): void {
+		this.render = (): ReactNode => {
+			return (
+				<div>
+					{this.childRender()}
+				</div>
+			);
+		};
+	}
+}
+
+interface IContainerProps {
+
+}
+
+interface IContainerState {
+
+}
+
+export {Container, IContainerState, IContainerProps};
