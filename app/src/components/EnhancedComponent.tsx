12ea4c48cfd9d0c0e2326b3ea00c23bf349d7e1b
Perry Liao <perryliaohc@gmail.com>
2020-05-24 14:32:29 -0700

    create components/containers; create EnhancedComponent

---
+++ app/src/components/EnhancedComponent.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import {ReactNode} from "react";
+
+abstract class EnhancedComponent<P extends IEnhancedComponentProps = IEnhancedComponentProps, S extends IEnhancedComponentState = IEnhancedComponentState> extends React.PureComponent<P, S> {
+
+	public static defaultProps: IEnhancedComponentProps = {
+		/* add default props that belong to every component */
+	}
+
+	private readonly childRender: () => ReactNode;
+
+	protected constructor(props: P) {
+		super(props);
+		// @ts-ignore
+		this.state = {};
+
+		this.childRender = this.render;
+		this.wrapRender = this.wrapRender.bind(this);
+		this.wrapRender();
+	}
+
+	private wrapRender(): void {
+		this.render = (): ReactNode => {
+			return(
+				<div>
+					{this.childRender()}
+				</div>
+			);
+		};
+	}
+}
+
+interface IEnhancedComponentProps {
+
+}
+
+interface IEnhancedComponentState {
+
+}
+
+export {EnhancedComponent, IEnhancedComponentProps, IEnhancedComponentState};
