ffe08a3f808bd610008a594414e8158e8ccd1e5f
Perry Liao <perryliaohc@gmail.com>
2020-06-06 00:59:00 -0700

    add login screen

---
+++ app/src/containers/LoginScreen.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import {ReactNode} from "react";
+import "../Container.css";
+import {Container} from "./Container";
+import {TextButton} from "../components/buttons/TextButton";
+
+class LoginScreen extends Container {
+
+	public render(): ReactNode {
+		return (
+			<div>
+				<img/>
+				<TextButton
+					text={"Login"}
+					width={250}
+					height={50}
+					fontSize={24}
+					bold={true}
+					buttonColour={"#6236FF"}
+					buttonFocusedColour={"#431cd7"}
+					buttonHoverColour={"#815cff"}
+				/>
+				<TextButton
+					text={"Sign Up"}
+					width={250}
+					height={50}
+					fontSize={24}
+					bold={true}
+					buttonColour={"#7D7D7D"}
+					buttonFocusedColour={"#535353"}
+					buttonHoverColour={"#999999"}
+				/>
+			</div>
+		);
+	}
+}
+
+export {LoginScreen};
--- app/src/containers/TestScreens/index.ts
+++ app/src/containers/TestScreens/index.ts
@@ -2,12 +2,14 @@ import {TextButtonTest} from "./TextButtonTest";
 import {EmptyPage} from "./EmptyPage";
 import {ImageButtonTest} from "./ImageButtonTest";
 import {TextInputTest} from "./TextInputTest";
+import {LoginScreen} from "../LoginScreen";
 
 export enum testScreen {
 	EmptyPage,
 	TextButtonTest,
 	ImageButtonTest,
 	TextInputTest,
+	LoginScreen,
 }
 
 export const testScreenMap: { [key: string]: { pointer: any, name: string } } = {
@@ -15,4 +17,5 @@ export const testScreenMap: { [key: string]: { pointer: any, name: string } } = 
 	[testScreen.TextButtonTest]: {pointer: TextButtonTest, name: "Text Button Page"},
 	[testScreen.ImageButtonTest]: {pointer: ImageButtonTest, name: "Image Button Page"},
 	[testScreen.TextInputTest]: {pointer: TextInputTest, name: "Text Input Page"},
+	[testScreen.LoginScreen]: {pointer: LoginScreen, name: "Login"},
 }
