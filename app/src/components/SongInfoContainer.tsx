fa002ebafaf7356f1b26ee6e80b1b410d6d433c3
Breanne <breannehuang98@gmail.com>
2020-06-19 00:47:04 -0700

    Removed commented out code

--- app/src/components/SongInfoContainer.tsx
+++ app/src/components/SongInfoContainer.tsx
@@ -23,7 +23,6 @@ class SongInfoContainer extends EnhancedComponent<ISongInfoContainerProps, ISong
                     src={albumPicture}
                     height="74"
                     width="132"
-                    // alt="Album Image" // this generates a warning that "image" shouldn't be part of the description
                     alt={"Album"}
                 />
                 <p className={"song-description"}>{songDescription}</p>
