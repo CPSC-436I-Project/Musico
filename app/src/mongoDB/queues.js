db27459c742cdeec6fb497c322d68f0276a4bc64
g0u1b <g0u1b@ugrad.cs.ubc.ca>
2020-06-23 22:04:52 -0700

    moved songs to playlists - need mongoose for ID

--- app/src/mongoDB/queues.js
+++ app/src/mongoDB/queues.js
@@ -4,11 +4,8 @@ db.queues.insertMany([
     "channel": "Electronic",
     "queue": [
       {
-        "songName": "Lose Somebody",
-        "artists": ["Kygo", "OneRepublic"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=TmWih4XfRWY",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -16,11 +13,8 @@ db.queues.insertMany([
     "channel": "Rock",
     "queue": [
       {
-        "songName": "You Can't Always Get What You Want",
-        "artists": ["The Rolling Stones"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=Ef9QnZVpVd8",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -28,11 +22,8 @@ db.queues.insertMany([
     "channel": "Lo-fi",
     "queue": [
       {
-        "songName": "Get You The Moon",
-        "artists": ["Kina", "Snow"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=WTsmIbNku5g&list=PLOzDu-MXXLliO9fBNZOQTBDddoA3FzZUo&index=1",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -40,11 +31,8 @@ db.queues.insertMany([
     "channel": "Reggae",
     "queue": [
       {
-        "songName": "Who Knows",
-        "artists": ["Protoje"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=hzqFmXZ8tOE",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -52,11 +40,8 @@ db.queues.insertMany([
     "channel": "Jazz",
     "queue": [
       {
-        "songName": "The Sky is Crying",
-        "artists": ["Gary B.B. Coleman"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=71Gt46aX9Z4",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -64,11 +49,8 @@ db.queues.insertMany([
     "channel": "Classical",
     "queue": [
       {
-        "songName": "Clair de Lune",
-        "artists": ["Claude Debussy"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=CvFH_6DNRCY",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -76,11 +58,8 @@ db.queues.insertMany([
     "channel": "Country",
     "queue": [
       {
-        "songName": "Jolene",
-        "artists": ["Dolly Parton"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=Ixrje2rXLMA",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -88,11 +67,8 @@ db.queues.insertMany([
     "channel": "Hip-Hop",
     "queue": [
       {
-        "songName": "Fancy",
-        "artists": ["Iggy Azalea"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=O-zpOMYRi0w",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
@@ -100,12 +76,9 @@ db.queues.insertMany([
     "channel": "Rap",
     "queue": [
       {
-        "songName": "Money",
-        "artists": ["Cardi B"],
-        "numVotes": defaultNumVotes,
-        "src": "https://www.youtube.com/watch?v=Zj2cK8wymIA",
-        "requesterId": "5ef2b315ed4d61192f51f005"
+        // "songID": "x", // this will come from the playlist
+        "numVotes": defaultNumVotes
       }
     ]
   },
-])
+]);
