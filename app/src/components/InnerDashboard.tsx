90973fc4e67792673e67510671fa1bd7b6f3133a
g0u1b <g0u1b@ugrad.cs.ubc.ca>
2020-07-18 23:42:22 -0700

    add getter for top queue items

--- app/src/components/InnerDashboard.tsx
+++ app/src/components/InnerDashboard.tsx
@@ -5,6 +5,7 @@ import "./Components.css";
 import {DashboardSongInfo} from "./DashboardSongInfo";
 import {Image} from "./Image"
 import {GenreEnum} from "./index";
+import {ISidebarProps} from "./Sidebar";
 
 class InnerDashboard extends EnhancedComponent {
 
@@ -12,13 +13,58 @@ class InnerDashboard extends EnhancedComponent {
         ...EnhancedComponent.defaultProps,
     };
 
-    // NEW FN TO PULL DB INFO
+    private constructor(props: ISidebarProps) {
+        super(props);
+        this.state = {topSongs: []};
+        this.getTopSongsOnQueues = this.getTopSongsOnQueues.bind(this);
+    }
+
+    private getTopSongsOnQueues(): void {
+
+        console.log("inside getTopSongsOnQueues");
+
+        let topSongs: Song[] = [];
+        // fetch('/queues')                     // use this when deploying app
+        fetch('http://localhost:9000/queues')       // use this for now
+            .then(response => response.json())
+            .then(queues => {
+                return queues.map(function(obj: any) {
+                    return obj.queue;
+                });
+            })
+            .then(queue => {
+                queue.forEach(function (q: []) {
+                    let topSong: Song = {
+                        songName: "default",
+                        artists: ["shouldn't", "see", "this"],
+                        genre: GenreEnum.JAZZ,
+                        src: "",
+                        requesterID: 0,
+                        albumCover: "",
+                        numVotes: 0
+                    };
+                    q.forEach(function (song: Song) {
+                        if (song.numVotes > topSong.numVotes) {
+                            topSong = song;
+                        }
+                    });
+                    topSongs.push(topSong);
+                });
+                console.log(topSongs);
+
+                this.setState({topSongs: topSongs});
+            });
+    }
+
+    public componentDidMount(): void {
+        this.getTopSongsOnQueues();
+    }
 
     public render(): ReactNode {
         const audioWaveIcon = "https://img.icons8.com/nolan/64/audio-wave.png"
         return (
             <div className="inner_dashboard">
-                <div 
+                <div
                     style={{
                         display: "flex",
                         flexDirection: "row",
@@ -41,10 +87,21 @@ class InnerDashboard extends EnhancedComponent {
     }
 }
 
+interface Song {
+    songName: string,
+    artists: string[],
+    genre: GenreEnum,
+    src: string,
+    requesterID: any,
+    albumCover: string,
+    numVotes: number
+}
+
 export interface IInnerDashboardProps extends IEnhancedComponentProps {
 }
 
 export interface IInnerDashboardState extends IEnhancedComponentState {
+    topSongs: Song[];
 }
 
 export {InnerDashboard};
