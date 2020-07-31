04706d12c46460aa5d45e87d00ede1ef146a5951
g0u1b <g0u1b@ugrad.cs.ubc.ca>
2020-07-31 12:42:22 -0700

    buttons look ok but don't work

--- app/src/components/buttons/SidebarTextImageButton.tsx
+++ app/src/components/buttons/SidebarTextImageButton.tsx
@@ -3,39 +3,52 @@ import {ReactNode} from "react";
 import "./Button.css";
 import "../css/Sidebar.css";
 import {Button, IButtonProps, IButtonState} from "./Button";
-import {ITextButtonProps} from "./TextButton";
+import {ITextButtonProps, TextButton} from "./TextButton";
+import {EnhancedComponent} from "../EnhancedComponent";
+import {ImageButton} from "./ImageButton";
 
-class SidebarTextImageButton extends Button<ISidebarTextImageButtonProps, ISidebarTextImageButtonState> {
+class SidebarTextImageButton extends EnhancedComponent<ISidebarTextImageButtonProps, ISidebarTextImageButtonState> {
 
-	public static defaultProps: ISidebarTextImageButtonProps = {
-		...Button.defaultProps,
-		text: "Electronic",
-		icon: "https://img.icons8.com/ios-glyphs/30/000000/electronic-music.png",
-	};
+    public static defaultProps: ISidebarTextImageButtonProps = {
+        ...EnhancedComponent.defaultProps,
+        text: "Electronic",
+        icon: "https://img.icons8.com/ios-glyphs/30/000000/electronic-music.png",
+    };
 
-	protected constructor(props: ISidebarTextImageButtonProps) {
-		super(props);
-		this.state = {
-			...this.state,
-		};
-	}
+    protected constructor(props: ISidebarTextImageButtonProps) {
+        super(props);
+        // this.state = {};
+    }
 
-	public render(): ReactNode {
-		return (
-			<div className="sidebar-text-image-button">
-				<img
-					src={this.props.icon}
-					alt={this.props.text.concat(" icon")}
-					className="sidebar-text-image-button-icon"
-				/>
-				<p>{this.props.text}</p>
-			</div>
-		);
-	}
+    public render(): ReactNode {
+        return (
+            <div className="sidebar-text-image-button">
+                <div>
+                    <ImageButton
+                        width={30}
+						buttonColour={"#00000000"}
+						buttonHoverColour={"#00000000"}
+						src={this.props.icon}
+                        // alt={this.props.text.concat(" icon")}
+                        // className="sidebar-text-image-button-icon"
+                    />
+                </div>
+                {/*<div>*/}
+                    <TextButton
+                        // width={140}
+						buttonColour={"#00000000"}
+						buttonHoverColour={"#00000000"}
+						text={this.props.text}
+                    />
+                {/*</div>*/}
+            </div>
+        );
+    }
 }
 
 export interface ISidebarTextImageButtonProps extends IButtonProps, ITextButtonProps {
-	icon: string;
+    text: string;
+    icon: string;
 }
 
 export interface ISidebarTextImageButtonState extends IButtonState {
