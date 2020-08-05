d0a057e0f6f8d60ecaf8f91393f7ca6a1bbf8976
Perry Liao <perryliaohc@gmail.com>
2020-08-04 21:24:24 -0700

    copy inner dashboard into dashboard

--- app/src/containers/Dashboard.tsx
+++ app/src/containers/Dashboard.tsx
@@ -3,30 +3,128 @@ import {ReactNode} from "react";
 import "./Container.css";
 import {Container, IContainerProps, IContainerState} from "./Container";
 import {connect} from "react-redux";
-import { InnerDashboard } from "src/components/InnerDashboard";
 import { IStore } from "src/redux/initialStore";
+import {GenreEnum, Image, Song, TextButton} from "../components";
+import {DashboardSongInfo} from "../components/DashboardSongInfo";
+import {getCookie} from "../utility/cookies";
+import {API_URL} from "../utility/constants";
 
 class Dashboard extends Container<IDashboardProps, IDashboardState> {
-
 	public static mapStateToProps:(state: IStore, props: IDashboardProps) => IDashboardProps = (state: IStore, props: IDashboardProps) => {
 		return {
 			...props,
-			selectedGenre: state.chatRoomStore.selectedGenre
+			selectedGenre: state.chatRoomStore.selectedGenre,
 		};
 	}
 
-	constructor(props: any) {
+	protected constructor(props: any) {
 		super(props);
 
 		this.state = {
-			sidebarOpen: true
+			...this.state,
+			topSongs: [],
 		}
+
+		this.getTopSongsOnQueues = this.getTopSongsOnQueues.bind(this);
+		this.addTopSong = this.addTopSong.bind(this);
 	}
+
+	private getTopSongsOnQueues(): void {
+		const token = getCookie('auth-token');
+		fetch(API_URL + 'queues', {
+			method: 'GET',
+			headers: {
+				'auth-token': token
+			}
+		})
+			.then(response => response.json())
+			.then(queues => {
+				return queues.map(function (obj: any) {
+					return obj.queue;
+				});
+			})
+			.then(queueList => {
+				let queueRequests = queueList.map((queue: string[]) =>
+					this.addTopSong(queue));
+				Promise.all(queueRequests);
+			});
+	}
+
+	private addTopSong(queue: string[]): Promise<void> {
+		let that = this;
+		const token = getCookie('auth-token');
+		let topSong: Song = {
+			songName: "default",
+			genre: "Jazz",
+			src: "",
+			requesterID: 0,
+			albumCover: "",
+			numVotes: 0
+		};
+		return Promise.all(
+			queue.map((songID: string) => fetch(API_URL + 'songs/' + songID, {
+				method: 'GET',
+				headers: {'auth-token': token}
+			})))
+			.then((responses) => {
+				return Promise.all(responses.map(response => response.json()))
+			})
+			.then((songs: Song[]) => {
+				songs.forEach(function (song: Song) {
+					// @ts-ignore //lint error for string enums because they can't be reverse mapped
+					if (song !== null && song.numVotes > topSong.numVotes && Object.values(GenreEnum).includes(song.genre)) {
+						topSong = song;
+					}
+				});
+			})
+			.then(() => {
+				if (topSong.songName !== "default") {
+					let topSongs: Song[] = that.state.topSongs;
+					let updatedTopSongs: Song[] = topSongs.concat(topSong);
+					return that.setState({topSongs: updatedTopSongs});
+				}
+			})
+			.then(() => {
+				return Promise.resolve();
+			})
+			.catch((err) => {
+				console.log(err);
+				return Promise.reject();
+			});
+	}
+
+	private static createSongInfo(song: Song): ReactNode {
+		return (<DashboardSongInfo
+			key={song.songName + Math.random() * 10000}
+			genre={song.genre}
+			albumCover={song.albumCover}
+			songName={song.songName}
+		/>);
+	}
+
+	public componentDidMount(): void {
+		this.getTopSongsOnQueues();
+	};
 
 	public render(): ReactNode {
 		return (
 			<div className={"dashboard dashboard_lower dashboard-display"}>
-				<InnerDashboard/>
+				<div className="inner-dashboard">
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							justifyContent: "flex-start",
+						}}
+					>
+						<Image width={40} height={40} path={"https://img.icons8.com/nolan/64/audio-wave.png"}/>
+						<h2> Playing next </h2>
+					</div>
+					<div className="dashboard-trending">
+						{this.state.topSongs.map(Dashboard.createSongInfo)}
+					</div>
+				</div>
 			</div>
 		);
 	}
@@ -38,6 +136,7 @@ export interface IDashboardProps extends IContainerProps {
 
 export interface IDashboardState extends IContainerState {
 	sidebarOpen?: boolean;
+	topSongs: Song[];
 }
 
 // @ts-ignore
