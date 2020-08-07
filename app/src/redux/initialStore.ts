8deb9cae9fa38d2d7334a2e370bcc38c7fe3c9a3
g0u1b <g0u1b@ugrad.cs.ubc.ca>
2020-08-06 20:39:31 -0700

    redux

--- app/src/redux/initialStore.ts
+++ app/src/redux/initialStore.ts
@@ -3,42 +3,42 @@ import profilePlaceholder from "../icons/profile-placeholder.png";
 import {defaultSong} from "../utility/songs";
 
 export interface IStore {
-	popupStore: IPopUpStore;
-	roomStore: IRoomStore;
-	userStore: IUserStore;
-	sidebarStore: ISidebarStore;
-	musicSidebarStore: IMusicSidebarStore;
+    popupStore: IPopUpStore;
+    roomStore: IRoomStore;
+    userStore: IUserStore;
+    sidebarStore: ISidebarStore;
+    musicSidebarStore: IMusicSidebarStore;
 }
 
 /**
  * Initial values of redux state
  */
 export default {
-	popupStore: {
-		popupOpen: false,
-	},
-	roomStore: {
-		selectedGenre: null,
-		messages: [],
-		queue: [defaultSong],
-		currentlyPlaying: defaultSong,
-		startTime: null
-	},
-	userStore: {
-		userId: null,
-		username: null,
-		email: null,
-		profileImgSrc: profilePlaceholder,
-		requests: [],
-		likedSongs: [],
-		favouriteGenres: [],
-		channels: null,
-	},
-	sidebarStore: {
-		sidebarOpen: true
-	},
-	musicSidebarStore: {
-		musicSidebarOpen: true,
-		selectedGenre: null,
-	}
+    popupStore: {
+        popupOpen: false,
+    },
+    roomStore: {
+        selectedGenre: null,
+        messages: [],
+        queue: [defaultSong],
+        currentlyPlaying: defaultSong,
+        startTime: null
+    },
+    userStore: {
+        userId: null,
+        username: null,
+        email: null,
+        profileImgSrc: profilePlaceholder,
+        requests: [],
+        likedSongs: [],
+        favouriteGenres: [],
+        channels: [],
+    },
+    sidebarStore: {
+        sidebarOpen: true
+    },
+    musicSidebarStore: {
+        musicSidebarOpen: true,
+        selectedGenre: null,
+    }
 };
